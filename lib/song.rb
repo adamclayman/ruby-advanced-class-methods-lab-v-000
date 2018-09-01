efbea4c5724ae8baaee5aa1b7fab319c74fb906c
Adam Clayman <adam@clayman.org>
2018-09-01 16:25:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
 
   def self.create
     song = self.new
-    @@all << song
+    self.all << song
     song
   end
 
@@ -25,7 +25,7 @@ class Song
   def self.create_by_name(name)
     song = self.new
     song.name = name
-    @@all << song
+    self.all << song
     song
   end
 

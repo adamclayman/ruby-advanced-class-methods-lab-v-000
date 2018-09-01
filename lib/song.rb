79f2f8192c177ee861c16427a5eb4804f970b787
Adam Clayman <adam@clayman.org>
2018-09-01 15:56:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,11 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name = nil, artist_name = nil)
+    @name = name
+    @artist_name = artist_name
+  end
+
   def self.all
     @@all
   end
